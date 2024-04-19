8a7d9e33fd7c7b12449b7911ed7d3ce66f86254b
sugavana <59100150+sugavana@users.noreply.github.com>
2024-04-19 02:25:02 -0400

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# This checks if the number of arguments is correct
+# If the number of arguments is incorrect ( $# != 2) print error message and exit
+if [[ $# != 2 ]]
+then
+  echo "backup.sh target_directory_name destination_directory_name"
+  exit
+fi
+
+# This checks if argument 1 and argument 2 are valid directory paths
+if [[ ! -d $1 ]] || [[ ! -d $2 ]]
+then
+  echo "Invalid directory path provided"
+  exit
+fi
+
+# [TASK 1]
+targetDirectory=$1
+destinationDirectory=$2
+
+# [TASK 2]
+echo "The target directory is $targetDirectory"
+echo "The destination directory is $destinationDirectory"
+
+# [TASK 3]
+currentTS=$(date +%s)
+echo "currentTS is $currentTS"
+
+# [TASK 4]
+backupFileName="backup-$currentTS.tar.gz"
+echo "backupFileName is $backupFileName"
+
+# We're going to:
+  # 1: Go into the target directory
+  # 2: Create the backup file
+  # 3: Move the backup file to the destination directory
+
+# To make things easier, we will define some useful variables...
+
+# [TASK 5]
+origAbsPath="$(pwd)"
+echo "origAbsPath is $origAbsPath"
+
+# [TASK 6]
+cd $destinationDirectory
+destDirAbsPath="$(pwd)"
+echo "destDirAbsPath is $destDirAbsPath"
+
+# [TASK 7]
+cd $origAbsPath
+cd $targetDirectory
+echo "Current directory is $(pwd)"
+
+# [TASK 8]
+yesterdayTS=$(($currentTS - 24*60*60))
+echo "yesterdayTS is $yesterdayTS"
+
+declare -a toBackup
+
+for file in $(find . -type f) # [TASK 9]
+do
+  # [TASK 10]
+  if (( `date -r $file +%s` > $yesterdayTS ))
+  then
+  # [TASK 11]
+    toBackup+=($file)
+  fi
+done
+
+#echo ${toBackup[@]}
+
+# [TASK 12]
+tar -czvf $backupFileName ${toBackup[@]}
+
+# [TASK 13]
+mv $backupFileName $destDirAbsPath
+
+# Congratulations! You completed the final project for this course!
